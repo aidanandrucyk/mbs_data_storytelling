48afbfdd42a0ad0eee9f34666d45e796fa37d665
aidanandrucyk <aa1918@scarletmail.rutgers.edu>
2023-07-09 23:02:37 -0400

    no-code-quiz

---
+++ Platform.java
@@ -0,0 +1,16 @@
+public class Platform {
+    
+    String name;
+    int votes;
+    boolean customization;
+    boolean buildTime;
+    boolean cost;
+    boolean userFriendly;
+    boolean risk;
+    boolean appScale;
+    int matches;
+    String description;
+    String pros;
+    String cons;
+
+}
