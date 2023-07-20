84abdbf75c381be5c2f784c78e560be901573089
aidanandrucyk <aa1918@scarletmail.rutgers.edu>
2023-07-20 03:54:31 -0400

    JS: Dataset, sorting ratings, & switch to results

--- scripts/quiz_logic.js
+++ scripts/quiz_logic.js
@@ -1,95 +1,129 @@
-// Import dependencies
-const fs = require("fs")
-const csv = require("csvtojson")
-const { Parser } = require("json2csv")
-
-(async () => {
-
-    // Load the cars
-    const cars = await csv().fromFile("('./data/appDevToolRatings.csv");
-
-    // Show the cars
-    console.log(cars);
-
-    // Modify the cars
-    cars[0].Year = 1998;
-
-    // Saved the cars
-    const carsInCsv = new Parser({ fields: ["Year", "Make", "Model", "Length"] }).parse(cars);
-    fs.writeFileSync("cars.csv", carsInCsv);
-
-})();
-
-function readCSVFile() {
-    const fs = require('fs');
-        
-    fs.readFile('../data/appDevToolRatings.csv', function read(err, data) {
-        if (err) {
-            throw err;
+function createPlatforms() {
+    var platforms = {
+        "Shoutem": {
+            rating: 0,
+            ideal_proficiency: 2,
+            user_friendliness: 3,
+            cost: 3.3,
+            time: 2,
+            customization: 3,
+            minimum_proficiency: 2,
+        },
+        "Goodbarber": {
+            rating: 0,
+            ideal_proficiency: 3,
+            user_friendliness: 4,
+            cost: 3.33,
+            time: 3,
+            customization: 2,
+            minimum_proficiency: 2,
+        },
+        "Appypie": {
+            rating: 0,
+            ideal_proficiency: 1,
+            user_friendliness: 2,
+            cost: 2,
+            time: 1,
+            customization: 1,
+            minimum_proficiency: 1,
+        },
+        "Bubble": {
+            rating: 0,
+            ideal_proficiency: 4,
+            user_friendliness: 2,
+            cost: 4.3,
+            time: 4,
+            customization: 4,
+            minimum_proficiency: 3,
+        },
+        "VSCode": {
+            rating: 0,
+            ideal_proficiency: 5,
+            user_friendliness: 1,
+            cost: 1,
+            time: 5,
+            customization: 5,
+            minimum_proficiency: 5,
         }
-        const content = data;
-    
-        // Invoke the next step here however you like
-        console.log(content);   // Put all of the code here (not the best solution)
-        getPlatforms(content);   // Or put the next step in a function and invoke it
-    });
-}
+    };
 
-function getPlatforms(files){
-
-    var platforms = {};
-    platforms[1] = 4;
-
-    if (files.length > 0) {
-
-        // Selected file
-        var file = files[0];
-
-        // FileReader Object
-        var reader = new FileReader();
-
-        // Read file as string 
-        reader.readAsText(file);
-
-        // Load event
-        reader.onload = function (event) {
-
-            // Read file data
-            var csvdata = event.target.result;
-
-            // Split by line break to gets rows Array
-            var rowData = csvdata.split('\n');
-
-            // <table > <tbody>
-            var tbodyEl = document.getElementById('tblcsvdata').getElementsByTagName('tbody')[0];
-            tbodyEl.innerHTML = "";
-
-            // Loop on the row Array (change row=0 if you also want to read 1st row)
-            for (var row = 1; row < rowData.length; row++) {
-
-                // Insert a row at the end of table
-                var newRow = tbodyEl.insertRow();
-
-                // Split by comma (,) to get column Array
-                rowColData = rowData[row].split(',');
-
-                // Loop on the row column Array
-                for (var col = 0; col < rowColData.length; col++) {
-
-                    // Insert a cell at the end of the row
-                    var newCell = newRow.insertCell();
-                    newCell.innerHTML = rowColData[col];
-
-                }
-
-            }
-        };
-
-    } else {
-        alert("Please select a file.");
-    }
-    console.log(platforms[1])
+    console.log(platforms);
+    console.log(platforms["Shoutem"]);
+    console.log(platforms["Shoutem"]["cost"]);
     return platforms;
 }
 
-readCSVFile()
+function rankPlatforms(platforms) {
+    // @Uthera implement quiz logic
+    // change the "rating" of each as users press buttons
+    // return the platforms dict with updated ratings
+
+
+
+    return platforms;
+}
+
+function sortJsObject(dict) {
+    var keys = [];
+    for(var key in dict) { 
+       keys[keys.length] = key;
+     }
+     var values = [];     
+     for(var i = 0; i < keys.length; i++) {
+         values[values.length] = dict[keys [i]];
+     }
+     var sortedValues = values.sort(sortNumber);
+     console.log(sortedValues);
+}
+
+function sortNumber(a,b) {
+   return a - b;
+}
+
+function orderRankedPlatforms(platforms) {
+    var rankedPlatforms = [];
+    var plats = Object.keys(platforms);
+    dict = {}
+    for (const [key, value] of Object.entries(platforms)) {
+        dict[key] = value["rating"]
+      }
+    rankedPlatforms = sortJsObject(dict)
+    return(rankedPlatforms)
+}
+
+function submitQuiz() {
+    // Aidan & Uthara
+
+    document.getElementById("questions").style.display = 'none';
+    var x = document.getElementById("questions");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function displayInfo(rankedPlatforms) {
+    // Aidan
+}
+
+function quizLogic() {
+    return orderRankedPlatforms(rankPlatforms(createPlatforms()));
+}
+
+window.onload = function () {   
+    // submit quiz
+    const targetDiv = document.getElementById("questions");
+    const newDiv = document.getElementById("resultsPage");
+    const btn = document.getElementById("submit");
+    btn.onclick = function () {
+        if (targetDiv.style.display !== "none") {
+            targetDiv.style.display = "none";
+            newDiv.style.display = "block";
+        } else {
+            targetDiv.style.display = "block";
+            newDiv.style.display = "none";
+        }
+        displayInfo(quizLogic())
+    };
+};
---
+++ scripts/quiz_logic_temp.js
@@ -0,0 +1,82 @@
+// Import dependencies
+const csv = require('csv-parser')
+const fs = require('fs');
+const data = [];
+
+fs.createReadStream('');
+
+
+
+function readCSVFile() {
+    const fs = require('fs');
+        
+    fs.readFile('../data/appDevToolRatings.csv', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        const content = data;
+    
+        // Invoke the next step here however you like
+        console.log(content);   // Put all of the code here (not the best solution)
+        getPlatforms(content);   // Or put the next step in a function and invoke it
+    });
+}
+
+function getPlatforms(files){
+
+    var platforms = {};
+    platforms[1] = 4;
+
+    if (files.length > 0) {
+
+        // Selected file
+        var file = files[0];
+
+        // FileReader Object
+        var reader = new FileReader();
+
+        // Read file as string 
+        reader.readAsText(file);
+
+        // Load event
+        reader.onload = function (event) {
+
+            // Read file data
+            var csvdata = event.target.result;
+
+            // Split by line break to gets rows Array
+            var rowData = csvdata.split('\n');
+
+            // <table > <tbody>
+            var tbodyEl = document.getElementById('tblcsvdata').getElementsByTagName('tbody')[0];
+            tbodyEl.innerHTML = "";
+
+            // Loop on the row Array (change row=0 if you also want to read 1st row)
+            for (var row = 1; row < rowData.length; row++) {
+
+                // Insert a row at the end of table
+                var newRow = tbodyEl.insertRow();
+
+                // Split by comma (,) to get column Array
+                rowColData = rowData[row].split(',');
+
+                // Loop on the row column Array
+                for (var col = 0; col < rowColData.length; col++) {
+
+                    // Insert a cell at the end of the row
+                    var newCell = newRow.insertCell();
+                    newCell.innerHTML = rowColData[col];
+
+                }
+
+            }
+        };
+
+    } else {
+        alert("Please select a file.");
+    }
+    console.log(platforms[1])
+    return platforms;
+}
+
+readCSVFile()
